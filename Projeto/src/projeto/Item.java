860c9cc965e72725e44572d7e5ad0269ea7cf425
luisaRosa <luisasilveirarosa@gmail.com>
2016-11-08 18:22:50 -0200

    Enviando classe Item

---
+++ Projeto/src/projeto/Item.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto;
+
+/**
+ *
+ * @author Laboratorio
+ */
+public class Item {
+    
+    private int numero;
+    private int quantidade;
+    private double percDesconto;
+
+    /**
+     * @return the numero
+     */
+    public int getNumero() {
+        return numero;
+    }
+
+    /**
+     * @param numero the numero to set
+     */
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the quantidade
+     */
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    /**
+     * @param quantidade the quantidade to set
+     */
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    /**
+     * @return the percDesconto
+     */
+    public double getPercDesconto() {
+        return percDesconto;
+    }
+
+    /**
+     * @param percDesconto the percDesconto to set
+     */
+    public void setPercDesconto(double percDesconto) {
+        this.percDesconto = percDesconto;
+    }
+    
+    
+}
